e02821562b9eacb25346f6003fc355e4e37220c8
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-05-14 18:18:26 -0400

    Content Changes

--- Content/Python/CameraClient.py
+++ Content/Python/CameraClient.py
@@ -3,13 +3,18 @@ import numpy as np
 import cv2
 import asyncio
 import time
+import io
 
 
 def show_image(image, name):
-    width, height = (image.width, image.height)
-    packed_data = np.array(image.pixels, dtype=np.uint32)
-    image_data = packed_data.view(dtype=np.uint8).reshape((height, width, 4))  # Assuming ARGB
-    cv2.imshow(f"Camera {name}", image_data)
+    # width, height = (image.width, image.height)
+    # packed_data = np.array(image.pixels, dtype=np.uint32)
+    # image_data = packed_data.view(dtype=np.uint8).reshape((height, width, 4))  # Assuming ARGB
+    # cv2.imshow(f"Camera {name}", image_data)
+    # cv2.waitKey(1)
+    image_file = io.BytesIO(image.data)
+    img = cv2.imdecode(np.frombuffer(image_file.getvalue(), np.uint8), cv2.IMREAD_COLOR)
+    cv2.imshow(f"Camera {name}", img)
     cv2.waitKey(1)
 
 
@@ -21,7 +26,8 @@ def stream_images_sync():
 async def stream_images_async(camera_id):
     overall_count = 0
     count = 0
-    async for image in ts.stream_images(camera_id):
+    async for image in ts.stream_images(camera_id, compression_level=0):
+        # print(len(image.data))
         overall_count += 1
         if overall_count == 20:
             start = time.time()
@@ -30,11 +36,11 @@ async def stream_images_async(camera_id):
             end = time.time()
             diff = end - start
             print("Got {} images in {} seconds from camera {} ({} per second)".format(count, diff, camera_id, count / diff))
-        # show_image(image, camera_id)
+        show_image(image, camera_id)
 
 
 async def main():
-    await asyncio.gather(stream_images_async(0), stream_images_async(1), stream_images_async(2), stream_images_async(3))
+    await asyncio.gather(stream_images_async(3))
 
 
 if __name__ == "__main__":
