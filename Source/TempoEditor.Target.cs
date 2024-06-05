f697ae8db12549dbdffb8c8e2a02828aab650eff
Peter Melick <pete@temposimulation.com>
2024-06-05 15:52:21 -0400

    Update Target Settings

--- Source/TempoEditor.Target.cs
+++ Source/TempoEditor.Target.cs
@@ -8,8 +8,8 @@ public class TempoEditorTarget : TargetRules
 	public TempoEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("Tempo");
 
 		if (Platform == UnrealTargetPlatform.Win64)
