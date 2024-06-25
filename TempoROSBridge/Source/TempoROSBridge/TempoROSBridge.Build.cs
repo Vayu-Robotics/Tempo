83835900e420951c79855e6d044631bc61ecce7b
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-06-25 16:22:22 -0400

    Add TempoROS Submodule and TempoROSBridge Pluign

---
+++ TempoROSBridge/Source/TempoROSBridge/TempoROSBridge.Build.cs
@@ -0,0 +1,39 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TempoROSBridge : ModuleRules
+{
+	public TempoROSBridge(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				// Unreal
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				// Tempo
+				"TempoROS",
+				"TempoSensors",
+				"TempoGeographic",
+				"TempoTime",
+				"TempoVehicles",
+				"TempoCamera",
+				"TempoScripting",
+				"rclcpp",
+			}
+			);
+	}
+}
